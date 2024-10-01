9af9d941550162af4f8bd9d7d046fe1104ec4f40
longrict <125839240+longrict@users.noreply.github.com>
2024-10-01 17:48:20 -0400

    SQL and excel files added

---
+++ sql/removeallrecords.sql
@@ -0,0 +1,8 @@
+DELETE FROM billing;
+DELETE FROM reservation;
+DELETE FROM car;
+DELETE FROM model;
+DELETE FROM protections_coverages;
+DELETE FROM customer;
+DELETE FROM employees;
+DELETE FROM location;
